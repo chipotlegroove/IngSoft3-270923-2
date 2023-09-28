45db5076e9f922c15f0b200b65a9d4f6ce63930e
Jared Barojas <jaredbarojas90@gmail.com>
2023-09-27 18:42:44 -0700

    agregar multiplicacion

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from resta import *
 from suma import *
+from multiplicacion import *
 
 if __name__ == '__main__':
     lista_numeros = ['a', 'b', 'c', 'd', 'e']
     print(suma(lista_numeros))
     print(resta(lista_numeros))
+    print(multiplicacion(lista_numeros))
