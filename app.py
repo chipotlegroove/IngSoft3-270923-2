e814a0edf37885f6ee77df46c4ba769947c01e1f
Jogibeda <117044716+Jogibeda@users.noreply.github.com>
2023-09-28 11:31:09 -0700

    intento dos

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     print(suma(lista_numeros))
     print(resta(lista_numeros))
     print(multiplicacion(lista_numeros))
-    print("Hola jaja")
+    print("Hola jajajaja")
